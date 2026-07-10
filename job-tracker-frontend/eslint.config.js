ddfcee7b35c328076a9342e2da9b00d31a76fc32
Darshan <darshuuu808@gmail.com>
2026-07-10 15:54:54 +0530

    Day 21: React app setup and routing

---
+++ job-tracker-frontend/eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+];
